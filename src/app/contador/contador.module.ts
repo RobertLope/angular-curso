9cb927a8e8708e99d1bae79750a399d7ac252fab
Robert Lopez <robertlopezcontreras@gmail.com>
2022-07-08 14:12:56 +0200

    primer commit

---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ContadorComponente } from './contador/contador.component';
+
+
+
+@NgModule({
+    declarations:[
+        ContadorComponente
+    ],
+    exports: [
+        ContadorComponente
+    ]
+   
+})
+
+export class ContadorModule {}
